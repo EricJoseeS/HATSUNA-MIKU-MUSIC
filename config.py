bbf0907fdce171804b33158554e5f3eeda2eaefe
ERIC JOSE ALVES DE SOUZA <pauloconcerno@gmail.com>
2025-07-09 20:48:31 -0300

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-# config.py
-
-# ⚠️ Cole seu token do Discord abaixo, DENTRO DAS ASPAS.
-# Lembre-se de NUNCA compartilhar este token com ninguém.
-DISCORD_TOKEN = "MTM5MjYzOTYxNDUzMTAxMDc2Mg.GMxT1G.Jk__TybM0zDdFIFqYxBJ0xRRfqIMAPBZhkK8x8"
-
